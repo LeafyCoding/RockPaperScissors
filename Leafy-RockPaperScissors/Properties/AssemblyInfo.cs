0bfafad0f40769324ebca352997d468707525905
LeafyDev <will09600@gmail.com>
2016-02-12 18:19:48 +0100

    Added logo and build event.

--- Leafy-RockPaperScissors/Properties/AssemblyInfo.cs
+++ Leafy-RockPaperScissors/Properties/AssemblyInfo.cs
@@ -1,25 +1,39 @@
-﻿using System.Reflection;
-using System.Runtime.CompilerServices;
+﻿// -----------------------------------------------------------
+// This program is private software, based on C# source code.
+// To sell or change credits of this software is forbidden,
+// except if someone approves it from the LeafyCoding INC. team.
+// -----------------------------------------------------------
+// Copyrights (c) 2016 Leafy-RockPaperScissors INC. All rights reserved.
+// -----------------------------------------------------------
+
+#region
+
+using System.Reflection;
 using System.Runtime.InteropServices;
+
+#endregion
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
+
 [assembly: AssemblyTitle("Leafy-RockPaperScissors")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
 [assembly: AssemblyProduct("Leafy-RockPaperScissors")]
-[assembly: AssemblyCopyright("Copyright ©  2016")]
+[assembly: AssemblyCopyright("Copyright © LeafyCoding 2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
 // COM, set the ComVisible attribute to true on that type.
+
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
+
 [assembly: Guid("25a5167f-179d-4b5c-97f6-418881063f26")]
 
 // Version information for an assembly consists of the following four values:
@@ -32,5 +46,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
+
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
